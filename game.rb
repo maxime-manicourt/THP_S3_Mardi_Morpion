d57aba9c71dfe3b8e0203a72f2e49a971db73b7b
maxime-manicourt <maxime.manicourt@gmail.com>
2018-07-17 23:50:41 +0200

    modif du game pour demander noms des joueurs

--- game.rb
+++ game.rb
@@ -2,21 +2,37 @@
 # load 'board_cases.rb'
 require_relative 'players.rb'
 
+class Players
+  # TO DO : la classe a 2 attr_accessor, son nom, sa valeur (X ou O).
+  # Elle a un attr_writer : il a gagne ?
+  attr_accessor :nom, :valeur
+  attr_writer :result
+
+  def initialize(nom, valeur)
+    # TO DO : doit regler son nom, sa valeur, son etat de victoire
+    @nom = nom
+    @valeur = valeur
+    # @result = result
+  end
+
+end
 
 # Classe qui lance le jeu
 class Game
 
-  def initialize
+  def initialize_game
     # TO DO : créé 2 joueurs, créé un board
     # @plateau = Board.new
-    @joueur1 = Players.new("Tic", "O")
-    @joueur2 = Players.new("Tac", "X")
-
+    puts "Entrez le nom du joueur 1 :"
+    name1 = gets.chomp
+    @joueur1 = Players.new(name1, 'O')
+    puts "Entrez le nom du joueur 2 :"
+    name2 = gets.chomp
+    @joueur2 = Players.new(name2, 'X')
   end
 
   def go
     # TO DO : lance la partie
-    puts "Hello #{@joueur1}"
   end
 
   def turn
@@ -25,5 +41,5 @@ class Game
   end
 
 end
-Game.new.initialize
-Game.new.go
+Game.new.initialize_game
+# Game.new.go
