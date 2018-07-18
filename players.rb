b2e20b087c3bb111e5918e6a074076c2f73e49f9
Manon Lan <manondoublier@yahoo.fr>
2018-07-18 08:33:10 +0200

    mise a jour du boulot de nuit

--- players.rb
+++ players.rb
@@ -1,17 +1,16 @@
-load 'board.rb'
-load 'game.rb'
+require_relative 'board.rb'
 
 class Players
-  # TO DO : la classe a 2 attr_accessor, son nom, sa valeur (X ou O).
-  # Elle a un attr_writer : il a gagne ?
-  attr_accessor :nom, :valeur
-  attr_writer :result
-
-  def initialize(nom, valeur)
-    # TO DO : doit regler son nom, sa valeur, son etat de victoire
-    @nom = nom
-    @valeur = valeur
-    # @result = result
-  end
-
-end
+  attr_accessor: :player :name 
+  #Demander le nom des joueur et leur attribuer un symbole
+board = Board.new
+puts 'Coucou, vous voulez jouer au Morpion? Joueur 1 quel est ton nom?'
+name1 = gets.chomp
+player1 = Player.new(name1, 'X')
+puts "Super, #{name1}. Tu as le symbole 'X'"
+sleep 1
+puts 'Et toi, joueur 2, quel est ton nom?'
+name2 = gets.chomp
+player2 = Player.new(name2, 'O')
+puts "Cool, ton symbole est 'O'"
+sleep 1
